59368d1d73c18a1ae97a86413a18bfb9525c5e51
Mahantesh Sanakall <msanak@codeaurora.org>
2019-01-03 18:49:39 +0530

    a5ultexx: Do not enable broadcastradio by default
        - Remove broadcast radio entries from manifest.xml
          and device.mk.
    
    Change-Id: Ibf0f52fb487512ced58903d5d2b0dae7d9c7254c

--- device.mk
+++ device.mk
@@ -41,7 +41,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.audio.effect@2.0-service \
-    android.hardware.broadcastradio@1.0-impl \
     audio.a2dp.default \
     audio_amplifier.msm8916 \
     audio.primary.msm8916 \
