c3f84cbcbe30e1ade8aa09829fbe3dfa2baab204
Kyle Harrison <khwebmail@gmail.com>
2019-09-13 12:51:03 +0100

    a5ultexx: nuke thermal hal

--- device.mk
+++ device.mk
@@ -336,10 +336,6 @@ $(call inherit-product, device/samsung/a5ultexx/system_prop.mk)
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
-
 # Trust
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
