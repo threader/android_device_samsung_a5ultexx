96ba815fe569b27e4feaf8c16c066f58e2f34f65
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-03-23 01:07:38 +0100

    a5ultexx: update DRM HIDL

--- device.mk
+++ device.mk
@@ -132,7 +132,8 @@ PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.2-service.clearkey \
+    android.hardware.drm@1.1-service.widevine
 
 # For config.fs
 PRODUCT_PACKAGES += \
